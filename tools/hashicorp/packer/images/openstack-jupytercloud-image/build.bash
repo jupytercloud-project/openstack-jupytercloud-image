9997c6895f14024970fcd3b151f10f5b1ca92af1
tannevaled <tannevaled@users.noreply.github.com>
2019-05-06 16:11:49 +0200

    Update build.bash

--- tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
+++ tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
@@ -7,6 +7,7 @@ image_name_base="$(basename ${packer_project})"
 
 function ::Packer.build() {
   #PACKER_LOG_PATH=${packer_log_path} \
+  wget "${OS_CACERT}"
   PACKER_LOG=1 \
   packer build \
     ${packer_project}/config-openstack.json

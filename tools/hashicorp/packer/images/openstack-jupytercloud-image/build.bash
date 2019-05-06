46f9e69ca7635af1bf873eec5b85fa1f838a7188
tannevaled <tannevaled@users.noreply.github.com>
2019-05-06 13:28:11 +0200

    Update build.bash

--- tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
+++ tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
 set -x
+
+function ::Packer.build() {
+  #PACKER_LOG_PATH=${packer_log_path} \
+  PACKER_LOG=1 \
+  packer build \
+    -var-file ${packer_project}/run/release.json \
+    ${packer_project}/config-openstack.json \
+}
+
+function ::Main () {
+  ::Packer.build
+}
+
+::Main

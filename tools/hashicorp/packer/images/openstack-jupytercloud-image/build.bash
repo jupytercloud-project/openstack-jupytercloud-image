be6fcaba9c4ab360c2aeac2c4dbada9900bb87e2
tannevaled <tannevaled@users.noreply.github.com>
2019-05-06 13:57:02 +0200

    Update build.bash

--- tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
+++ tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
@@ -9,11 +9,6 @@ function ::Packer.build() {
   #PACKER_LOG_PATH=${packer_log_path} \
   PACKER_LOG=1 \
   packer build \
-    -var "openstack_identity_endpoint=${OPENSTACK_IDENTITY_ENDPOINT}" \
-    -var "openstack_domain_name=${OPENSTACK_DOMAIN_NAME}" \
-    -var "openstack_tenant_name=${OPENSTACK_TENANT_NAME}" \
-    -var "openstack_username=${OPENSTACK_USERNAME}" \
-    -var "openstack_password=${OPENSTACK_PASSWORD}" \
     ${packer_project}/config-openstack.json
 }
 

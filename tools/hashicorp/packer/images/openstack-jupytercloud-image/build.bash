efcbda64f571af86b37aa7d0d0272ad7f051ddba
tannevaled <tannevaled@users.noreply.github.com>
2019-05-13 15:03:30 +0200

    Update build.bash

--- tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
+++ tools/hashicorp/packer/images/openstack-jupytercloud-image/build.bash
@@ -5,17 +5,36 @@ set -x
 packer_project="$(dirname ${BASH_SOURCE})"
 image_name_base="$(basename ${packer_project})"
 
+function ::CACert.fetch() {
+  curl --location \
+       --output ./cacert.pem \
+       "${OS_CACERT}"
+}
+
+function ::Image.upload() {
+  openstack --version
+  #
+  # make it work even with openstack installation without http image source
+  #
+  curl --output openstack-alpine-image-3.9.3.raw \
+       --location ${SRC}
+  openstack --os-cacert ./cacert.pem \
+            --os-image-api-version 2 \
+            image create \
+            --file ./openstack-alpine-image-3.9.3.raw \
+            openstack-alpine-image-3.9.3
+}
+
 function ::Packer.build() {
   #PACKER_LOG_PATH=${packer_log_path} \
-  curl --location \
-       --output cacert.pem \
-       "${OS_CACERT}"
   PACKER_LOG=1 \
   packer build \
     ${packer_project}/config-openstack.json
 }
 
 function ::Main () {
+  ::CACert.fetch
+  ::Image.upload
   ::Packer.build
 }
 
